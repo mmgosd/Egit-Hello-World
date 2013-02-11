dcbc056b55dfea8ea6f95518e7b8ec57c7240fea
michael.martin <michael.martin@solvedirect.com>
2013-02-11 11:20:48 -0800

    Initial Create

---
+++ helloworld/src/com/solvedirect/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.solvedirect.helloworld;
+
+/**
+ * @author Michael
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+
+	}
+
+}
